ee0b1bfcc56ba745075c5a4cd9deb9038845c06f
alerner1 <alyssaplerner@gmail.com>
2021-05-10 18:15:57 -0400

    added assignment to comments in controller for easy access

--- app/controllers/merchant_config_controller.rb
+++ app/controllers/merchant_config_controller.rb
@@ -1,4 +1,12 @@
 class MerchantConfigController < ApplicationController
   def submit_merchant_config
+    # requires:
+    # maximum_loan_amount (int, in cents)
+    # minimum_loan_amount (int, in cents)
+    # prequal_enabled (boolean)
+
+    # if valid, update MerchantConfiguration accordingly
+    # return 200
+    # if invalid, return 400 if merchant does not exist
   end
 end
