c1dc45db9a488954832bc80ab8a50c699ae02ed6
alerner1 <alyssaplerner@gmail.com>
2021-05-10 19:35:43 -0400

    got rswag set up to test submit_merchant_config

--- app/controllers/merchant_config_controller.rb
+++ app/controllers/merchant_config_controller.rb
@@ -1,5 +1,11 @@
 class MerchantConfigController < ApplicationController
   def submit_merchant_config
+    if params[:id] == "1"
+      render json: {message: 'yup'}, status: :ok
+    else 
+
+      render json: {message: 'hi'}, status: :ok
+    end
     # requires:
     # maximum_loan_amount (int, in cents)
     # minimum_loan_amount (int, in cents)
@@ -8,5 +14,7 @@ class MerchantConfigController < ApplicationController
     # if valid, update MerchantConfiguration accordingly
     # return 200
     # if invalid, return 400 if merchant does not exist
+
+    # also needs a merchant_id, obviously
   end
 end
---
+++ spec/requests/merchant_configuration_spec.rb
@@ -0,0 +1,20 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/merchantconfig', type: :request do
+  path "/api/v1/merchantconfig/{id}" do
+    post "create merchant configuration" do 
+      consumes "application/json"
+      parameter name: :id, in: :path, type: :string
+      parameter name: :merchantconfig, in: :body, schema: {"$ref" => "#/components/schemas/CreateMerchantConfigRequest"}
+
+      response "200", "OK" do
+        let(:merchantconfig) { { minimum_amount: 2, maximum_amount: 58, prequal_enabled: true } }
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          expect(data["message"]).to eq("hi")
+        end
+      end
+    end
+
+  end
+end
