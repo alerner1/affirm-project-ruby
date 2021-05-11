d5f7850b1c30aced35bdff0b4a410ee3c64125b0
alerner1 <alyssaplerner@gmail.com>
2021-05-11 13:52:07 -0400

    added validators for minimum and maximum loan amounts greater than zero and min not larger than max

--- app/controllers/merchant_config_controller.rb
+++ app/controllers/merchant_config_controller.rb
@@ -15,7 +15,7 @@ class MerchantConfigController < ApplicationController
     # create a new instance for validation purposes (before updating/saving instance that's already in db). 
     # as far as I can tell this is essentially what ActiveRecord's update method does anyway (to update without saving). 
     new_merchant_conf = MerchantConfiguration.new(merchant_conf.merchant_id, merchant_conf.merchant_name, params[:minimum_loan_amount], params[:maximum_loan_amount], params[:prequal_enabled])
-
+    
     if new_merchant_conf.invalid?
       response = {
         message: "Invalid request."
--- app/models/merchant_configuration.rb
+++ app/models/merchant_configuration.rb
@@ -9,13 +9,17 @@ class MerchantConfiguration
 
   validates :merchant_id, presence: true
   validates :merchant_name, presence: true
-  validates :minimum_loan_amount, presence: true, numericality: true
-  validates :maximum_loan_amount, presence: true, numericality: true
+
+  # added validator to check that minimum loan amount is less than maximum loan amount
+  # added validators to check that loan amounts are both greater than zero 
+  validates :minimum_loan_amount, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: :maximum_loan_amount }
+  validates :maximum_loan_amount, presence: true, numericality: { greater_than: 0 }
+
   # added validation for prequal_enabled boolean
   validates :prequal_enabled, presence: true, inclusion: [true, false]
 
   # added prequal_enabled to schema
-  def initialize(merchant_id, merchant_name, minimum_loan_amount, maximum_loan_amount, prequal_enabled)
+  def initialize(merchant_id, merchant_name, minimum_loan_amount, maximum_loan_amount, prequal_enabled=false)
     @merchant_id = merchant_id
     @merchant_name = merchant_name
     @minimum_loan_amount = minimum_loan_amount
@@ -28,4 +32,5 @@ class MerchantConfiguration
     self.maximum_loan_amount = maximum_loan_amount
     self.prequal_enabled = prequal_enabled
   end
+  
 end
